70fb06836474fb18195fd82f38ac56ff5a0b819e
Federico Alliani <fedealliani@gmail.com>
2016-08-20 14:32:49 -0300

    ct_New pero no va agregarle el nodo root

--- solucion/cuatrotree.asm
+++ solucion/cuatrotree.asm
@@ -1,3 +1,14 @@
+%define NULL 0
+%define offset_arbol_root 0
+%define offset_arbol_size 8
+%define offset_nodo_father 0
+%define offset_nodo_valorUno 8
+%define offset_nodo_valorDos 12
+%define offset_nodo_valorTres 16
+%define offset_nodo_len 20
+%define offset_nodo_hijo 21
+
+
 ; FUNCIONES de C
   extern malloc
   extern free
@@ -19,6 +30,25 @@ section .text
 ; =====================================
 ; void ct_new(ctTree** pct);
 ct_new:
+;Recibo el parametro de C
+  mov r8,[rdi] ; Me llego por RDI ctTree** pct --> r8= Direccion del struct.
+  ;Libero memoria para el nodo root
+  mov rdi,29 ; Muevo a rd1 29 , porque voy a liberar 29 bytes para el nodo root
+  call malloc ; Libero 12 bytes y me devuelve la direccion en RAX =Direccion comienzo nodo root struct
+  ;Defino el arbol, es decir, le pongo el puntero al root y cantida de hijos =0
+  mov [r8+offset_arbol_root],rax ; Pongo en los primeros 8 bytes la direccion del nodo root
+  mov r9,0
+  mov [r8+offset_arbol_size],r9 ; Cantidad de hijos =0
+; Ahora pongo el nodo root todo en null
+  mov r9,r8
+  mov r8,[r9];Tengo en r8 la direccion de inicio del nodo root
+  mov r9,NULL
+  mov  [r8+offset_nodo_father],r9 ; Nodo padre = null
+  mov [r8+offset_nodo_valorUno],r9d; Si no tiene hijos va 0 ??
+  mov  [r8+offset_nodo_valorDos],r9d; Si no tiene hijos va 0 ??
+  mov [r8+offset_nodo_valorTres],r9d; Si no tiene hijos va 0 ??
+  mov  [r8+offset_nodo_len],r9b; CAMBIAR EL MOV PARA QUE SEA UN BYTES 
+  mov  [r8+offset_nodo_hijo],r9; 
         ret
 
 ; =====================================
--- solucion/cuatrotree.c
+++ solucion/cuatrotree.c
@@ -1,8 +1,15 @@
 #include "cuatrotree.h"
+ctNode* ct_aux_search(ctNode** currNode, ctNode* fatherNode, uint32_t newVal){
 
-void ct_new(ctTree** pct){
-	
+ctNode* h;
+return h;
+}
+
+void ct_aux_fill(ctNode* currNode, uint32_t newVal){
+
 }
 
 void ct_add(ctTree* ct, uint32_t newVal) {
-}
+	
+	}
+
