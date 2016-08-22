abd5d46100e007acf426507ff7c33233975d07d8
Federico Alliani <fedealliani@gmail.com>
2016-08-22 18:09:46 -0300

    Mas funciones!! :D

--- solucion/cuatrotree.asm
+++ solucion/cuatrotree.asm
@@ -6,7 +6,10 @@
 %define offset_nodo_valorDos 12
 %define offset_nodo_valorTres 16
 %define offset_nodo_len 20
-%define offset_nodo_hijo 21
+%define offset_nodo_hijo1 21
+%define offset_nodo_hijo2 29
+%define offset_nodo_hijo3 37
+%define offset_nodo_hijo4 45
 %define offset_iter_arbol 0
 %define offset_iter_nodo 8
 %define offset_iter_current 16
@@ -79,12 +82,40 @@ ctIter_delete:
 ; =====================================
 ; void ctIter_first(ctIter* ctIt);
 ctIter_first:
+;ALINEADA
+  push rbp ;D
+  mov rbp,rsp
+  push rbx ;A
+  push r12 ;D
+  push r13 ;A
+  push r14 ;D
+  push r15 ;A
   mov r8,rdi ;En r8 tengo la direccion iterador
   mov r9,[r8+offset_iter_arbol] ; En r9 tengo la direccion del arbol
   mov r10,[r9+offset_arbol_root] ; En r10 tengo la direccion nodo root del arbol
-  ;!!!! TENGO QUE RECORRER DEL MAS CHICO AL MAS GRANDE? !!!!!
-
-        ret
+  mov r12,r10
+  mov r13,r10
+  ciclo:
+  ;r12 es el nodo temporal a preguntar
+  ;r13 es el ultimo nodo que tiene valor
+  ;if(nodo.hijo[0]!=null)
+  mov r13,r12 ; Muevo la direccion del nodo a r13
+  mov r12,[r13+offset_nodo_hijo1] ;Muevo la direccion del nodo hijo1 para ver si no es NULL
+  cmp r12,NULL
+  jnz ciclo ;Si no es null sigo en el ciclo
+  ;Sali del ciclo y tengo en r13 el ultimo nodo valido
+  mov [r8+offset_iter_nodo],r13
+  mov r14b,1
+  mov [r8+offset_iter_current],r14b
+  mov r14d,0
+  mov [r8+offset_iter_count],r14d
+  pop r15;D
+  pop r14;A
+  pop r13;D
+  pop r12;A
+  pop rbx;D
+  pop rbp;A
+  ret
 
 ; =====================================
 ; void ctIter_next(ctIter* ctIt);
@@ -139,19 +170,22 @@ end:
 ; =====================================
 ; uint32_t ctIter_get(ctIter* ctIt);
 ctIter_get:
+  mov r9,[rdi+offset_iter_nodo] ;En r9 tengo la direccion del nodo actual
+  mov r10b,[rdi+offset_iter_current]; En r10 tengo la posicion actual dentro del nodo
+  mov eax, [rdi+r10b*4+8] ; Guardo el resultado en eax(32 bits bajos de rax)
         ret
 
 ; =====================================
 ; uint32_t ctIter_valid(ctIter* ctIt);
 ctIter_valid:
   mov r9,NULL
-  cmp rdi,r9
+  cmp [rdi+offset_iter_nodo],r9
   jz esInvalido
-  mov rax,[rdi+offset_iter_count] ;!!!!!!!PREGUNTAR QUE HAY QUE DEVOLVER!!!!!
+  mov eax,1 ;!!!!!!!PREGUNTAR QUE HAY QUE DEVOLVER!!!!!
 fin:
         ret
 esInvalido:
-  mov rax,r9
+  mov eax,r9
   jmp fin
 
 
